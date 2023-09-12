d5dd11163a9fd3e0a824615e7cd15917220e0a82
minhtycode <ty88611@gmail.com>
2023-09-12 22:28:53 +0700

    [Hook] - Add hook usePrevious

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -3,7 +3,7 @@ export { default as useCountdown } from "./useCountdown";
 export { default as useDebounce } from "./useDebounce";
 export { default as useLanguage } from "./useLanguage";
 export { default as useOnClickOutside } from "./useOnClickOutside";
+export { default as usePrevious } from "./usePrevious";
 export { default as useResponsive } from "./useResponsive";
 export { default as useSyncUrlWithTab } from "./useSyncUrlWithTab";
 export { default as useToggle } from "./useToggle";
-
